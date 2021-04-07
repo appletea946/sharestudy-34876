ba853f7a187e96f3258b80144fe35a35894343f1
appletea946 <shouyisheng@gmail.com>
2021-04-07 12:29:43 +0900

    記事お気に入りの非同期通信

--- app/javascript/like_article.js
+++ app/javascript/like_article.js
@@ -4,7 +4,13 @@ function like_article(){
 
   article_like.addEventListener("click", (e) => {
     e.preventDefault();
-
+    const form = document.getElementById("article-form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    const article_id = document.getElementById("like_article_article_id").value;
+    XHR.open("POST", `/articles/${article_id}/like_articles`, true);
+    XHR.responseType = "json";
+    XHR.send(formData);
   })
 
   article_not_like.addEventListener("click", (e) => {
